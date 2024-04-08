ab28f6a976186f5434a0b4a3edbc5ff346b5ccbf
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-04-08 16:35:53 +0200

    implemented login in auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
     try {
@@ -37,24 +38,52 @@ export const signup = async (req, res) => {
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
 
-        await newUser.save();
+        
+        if(newUser) { 
+            
+            generateTokenAndSetCookie(newUser._id, res);
 
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        })
+            await newUser.save();
 
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            })
+        } else {
+            res.status(400).json({ error: "Invalid user data." })
+        }
+    
     } catch (error) {
         console.log("Error in signup controller", error.message)
         res.status(500).json({error: "Internal Server Error"})
     }
 }
 
-export const login = (req, res) => {
-    res.send('LOGIN')
-    console.log("LOGIN")
+export const login = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username })
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+
+        if(!user || !isPasswordCorrect) {
+            return res.status(400).json({ error: "Invalid username or password." })
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic
+        })
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message)
+        res.status(500).json({error: "Internal Server Error"})
+    }
 }
 
 export const logout = (req, res) => {
