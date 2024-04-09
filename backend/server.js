10a392412bcee0b5141407be9bc886916828640b
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-04-09 15:22:34 +0200

    implemented message, senderId and receiverId

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 import express, { json } from 'express'
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT)
