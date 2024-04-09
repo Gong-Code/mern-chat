15b6fd7292a9296f0ea9ba3ef76349b5f8cb0831
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-04-09 15:41:43 +0200

    Add getMessages route and controller function

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -40,3 +40,20 @@ export const sendMessage = async (req, res) => {
         res.status(500).json({error: "Internal Server Error"})
     }
 }
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] }
+        }).populate('messages');
+
+        res.status(200).json(conversation.messages)
+
+    } catch (error) {
+        console.log("Error in getMessages controller", error.message)
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}
